55b320310688c0346cea03475bd516f0e33988d9
atul singh <atulsingh0622@gmail.com>
2020-09-14 23:46:25 +0530

    login page with db detail

--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace billingProject
 {
@@ -17,5 +11,38 @@ namespace billingProject
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = D:\billing\billing.mdf; Integrated Security = True; Connect Timeout = 30");
+            con.Open();
+            String query = "Select Count(*) From login where usename='" + textBox1.Text + "' and password ='" + textBox2.Text + "'";
+            SqlCommand sqlCmd = new SqlCommand(query);
+            String output = sqlCmd.ExecuteScalar().ToString();
+            if (output == "1")
+            {
+                Form2 f = new Form2();
+                f.Show();
+            }
+            else
+            {
+                this.Hide();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
---
+++ dbo.Table.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[login]
+(
+	[username] VARCHAR(50) NOT NULL PRIMARY KEY, 
+    [password] VARCHAR(50) NOT NULL
+)
